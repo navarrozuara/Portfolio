2a323d29fe8eea81a175d4f936d5a64061ec0d71
navarrozuara <navarrozuara@gmail.com>
2016-02-13 17:53:15 +0100

    Modificado error del script

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 $(function() {
 	var pos;
 	
-	$(".about").click(function() {
+	$(".about").click(function(evento) {
+		evento.preventDefault();
 		pos = $("#about").offset().top;
 		$("html, body").animate({
 			scrollTop: pos
 		}, 2000);
 	});
 
-	$(".work").click(function() {
+	$(".work").click(function(evento) {
+		evento.preventDefault();
 		pos = $("#work").offset().top;
 		$("html, body").animate({
 			scrollTop: pos
 		}, 2000);
 	});
 
-	$(".contact").click(function() {
+	$(".contact").click(function(evento) {
+		evento.preventDefault();
 		pos = $("#contact").offset().top;
 		$("html, body").animate({
 			scrollTop: pos
